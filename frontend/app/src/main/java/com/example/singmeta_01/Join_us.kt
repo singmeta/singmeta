dd17b755769acb41908ce9878ae5c6464d544909
jinhobyeong <jhb_0921@naver.com>
2022-03-20 18:49:21 +0900

    Feat : phoneNumber_retrofit

--- frontend/app/src/main/java/com/example/singmeta_01/Join_us.kt
+++ frontend/app/src/main/java/com/example/singmeta_01/Join_us.kt
@@ -1,17 +1,31 @@
 package com.example.singmeta_01
 
-import android.content.Context
+
 import android.content.Intent
-import android.net.Uri
+
 import android.os.Bundle
-import android.telephony.SmsManager
+
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
-class Join_us : AppCompatActivity() {
+class Join_us : AppCompatActivity(), View.OnClickListener{
+
+    //레트로핏 추가 ⭐️
+    private val TAG = "MainActivityLog"
+    private val URL = "http://192.168.0.113:3000"
+    private lateinit var retrofit: Retrofit
+    private lateinit var service: ApiService
 
     lateinit var register_user_email: EditText
     lateinit var register_user_password: EditText
@@ -26,6 +40,9 @@ class Join_us : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.join_us)
 
+
+
+
         var isExistBlank = false
         //var isPWSame = false
 
@@ -37,11 +54,14 @@ class Join_us : AppCompatActivity() {
         btn_register = findViewById(R.id.Btn_register)
         btn_certification = findViewById(R.id.Btn_certification)
 
-        btn_certification = findViewById(R.id.Btn_certification)
+        firstInit()
+        btn_certification.setOnClickListener(this)
 
-        btn_certification.setOnClickListener{
-
-        }
+        //❌
+//        btn_certification.setOnClickListener{
+//            //이 버튼 누르면 전화번호 넘어가게 설정
+//
+//        }
 
         btn_register.setOnClickListener {
            // Log.d(TAG, "회원가입 버튼 클릭")
@@ -87,6 +107,73 @@ class Join_us : AppCompatActivity() {
             }
 
         }
+    }
+
+
+
+
+
+
+    fun firstInit() {
+        btn_certification = findViewById<View>(R.id.Btn_certification) as Button
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        service = retrofit.create(ApiService::class.java)
+    }
+
+
+
+
+
+    /**
+     * View.OnLongClickListener override method
+     */
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.Btn_certification -> {
+                val call_get = service!!.getFunc(register_user_phone.text.toString())
+                call_get.enqueue(object : Callback<ResponseBody> {
+                    override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                    ) {
+                        if (response.isSuccessful) {
+                            try {
+                                val result = response.body()!!.string()
+                                Log.v(TAG, "result = $result")
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT)
+                                    .show()
+                            } catch (e: IOException) {
+                                e.printStackTrace()
+                            }
+                        } else {
+                            Log.v(TAG, "error = " + response.code().toString())
+                            Toast.makeText(
+                                applicationContext,
+                                "error = " + response.code().toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                        Log.v(TAG, "Fail")
+                        Toast.makeText(applicationContext, "Response Fail", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                })
+            }
+        }
+
+
+
+
+
+
     }
 
 //    fun sendSMS()
