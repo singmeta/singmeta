ae2ef1720fc943a85604cac1a99606bc55f27d21
sngyn <seye125@naver.com>
2022-03-21 22:09:37 +0900

    5, 6 xml updates

---
+++ frontend/app/src/main/java/com/example/singmeta_01/Room_setting.kt
@@ -0,0 +1,15 @@
+package com.example.singmeta_01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Room_setting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.room_setting)
+
+
+
+
+    }
+}
