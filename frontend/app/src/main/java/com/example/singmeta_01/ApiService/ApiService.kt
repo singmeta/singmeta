3cb0b3bdb2137556dec25222a019997451181765
jinhobyeong <jhb_0921@naver.com>
2022-03-26 22:12:43 +0900

    Feat : createRoom Api Add

--- frontend/app/src/main/java/com/example/singmeta_01/ApiService/ApiService.kt
+++ frontend/app/src/main/java/com/example/singmeta_01/ApiService/ApiService.kt
@@ -1,4 +1,4 @@
-package com.example.singmeta_01
+package com.example.singmeta_01.ApiService
 
 import okhttp3.ResponseBody
 import retrofit2.Call
--- frontend/app/src/main/java/com/example/singmeta_01/MainActivity.kt
+++ frontend/app/src/main/java/com/example/singmeta_01/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.singmeta_01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var test_btnRoom : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test_btnRoom = findViewById(R.id.Test_btnRoom)
+
+        test_btnRoom.setOnClickListener{
+            val intentId = Intent(applicationContext, Room_theme::class.java).run {
+                startActivity(this)
+            }
+        }
 
     }
 }
